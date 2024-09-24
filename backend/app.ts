f434e54dab0cef9284d83e7ae02dc9396c729111
Thereisnotruth <thereisnotruth12@gmail.com>
2024-09-24 10:39:28 +0900

    change host, port

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from 'express'
 
 const app: Express = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Typescript + Node.js + Express Server')
 })
 
-app.listen(port, () => {
+app.listen(Number(port), '0.0.0.0', () => {
   console.log(`[server]: Server is running at https://localhost:${port}`)
 })
