110acce58b737a74ab132ac700c1e69c62680006
Thereisnotruth <thereisnotruth12@gmail.com>
2024-11-05 00:10:41 +0900

    middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,7 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+function doesPathStartWithAny(path: string, substrings: string[]) {
+  return substrings.some((substring) => path.startsWith(substring))
+}
 export function middleware(req: NextRequest) {
-  console.log(req.nextUrl.pathname)
+  const pathname = req.nextUrl.pathname
+  if (
+    doesPathStartWithAny(pathname, [
+      '/api',
+      '/_next/static',
+      '/_next/image',
+      '/favicon.ico',
+      '/register',
+      '/mockServiceWorker.js',
+      '/manifest.json',
+      '/sw.js',
+      '/images',
+    ])
+  ) {
+    return NextResponse.next()
+  }
 
-  return NextResponse.next()
+  return NextResponse.redirect(new URL('/test/register', req.url))
 }
